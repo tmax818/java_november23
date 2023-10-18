902831da7c41f72bb17dd5508c17c543d37819ce
tmax818 <tylermaxwell661@gmail.com>
2023-10-18 11:33:13 -0600

    add demo mvn project sb

---
+++ prep/sandbox/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
