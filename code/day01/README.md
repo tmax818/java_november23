97aa1d6ea5d47b3eb172fd280c00746da00a2e38
tmax818 <tylermaxwell661@gmail.com>
2023-10-18 10:00:56 -0600

    update

--- code/day01/README.md
+++ code/day01/README.md
@@ -9,5 +9,27 @@
 ## data types
 
 - primitive and reference
+  - primitive types:
+
+      |type|size(bits)|
+      |---|---|
+      |boolean|1|
+      |byte|8|
+      |char|16|
+      |short|16|
+      |int|32|
+      |long|64|
+      |||
+      |float|32|
+      |double|64|
+
+  - reference types:
+    - anything that is not primitive...
+    - each primitive has a wrapper class that gives primitives OOP functionality
+    - [DataTypes](./DataTypes/Main.java)
+
+
+
+-----
 
 [^1]: Write once, run anywhere.
--- prep/day01/DataTypes/Main.java
+++ prep/day01/DataTypes/Main.java
@@ -31,13 +31,13 @@ public class Main {
 
 
 
-        // System.out.printf("Size of boolean in bits: %d\n", 1);
-        // System.out.printf("Size of byte in bits: %d\n", Byte.SIZE);
-        // System.out.printf("Size of short in bits: %d\n", Short.SIZE);
-        // System.out.printf("Size of int in bits: %d\n", Integer.SIZE);
-        // System.out.printf("Size of long in bits: %d\n", Long.SIZE);
-        // System.out.printf("Size of float in bits: %d\n", Float.SIZE);
-        // System.out.printf("Size of double in bits: %d\n", Double.SIZE);
+        System.out.printf("Size of boolean in bits: %d\n", 1);
+        System.out.printf("Size of byte in bits: %d\n", Byte.SIZE);
+        System.out.printf("Size of short in bits: %d\n", Short.SIZE);
+        System.out.printf("Size of int in bits: %d\n", Integer.SIZE);
+        System.out.printf("Size of long in bits: %d\n", Long.SIZE);
+        System.out.printf("Size of float in bits: %d\n", Float.SIZE);
+        System.out.printf("Size of double in bits: %d\n", Double.SIZE);
 
 
     }
