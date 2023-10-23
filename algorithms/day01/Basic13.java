092b0a6d5909216b2508d9bff6791b03595d33cc
tmax818 <tylermaxwell661@gmail.com>
2023-10-23 07:42:09 -0700

    update

---
+++ algorithms/day01/Basic13.java
@@ -0,0 +1,18 @@
+package algorithms.day01;
+
+public class Basic13 {
+
+// TODO: print all integers from 1 to 100
+
+// TODO: print even integers from 0 to 100
+
+// TODO: given an array, find its largest element
+
+// TODO: create an array with all odd inntegers between 1 and 100(inclusive)
+
+// TODO: replace any negative array values with "dojo"
+
+// TODO: find the average of a given array's values.
+    
+    
+}
