97032d540f7f0d88a427437ce9db4a80db86b64a
tmax818 <tylermaxwell661@gmail.com>
2023-10-24 08:39:03 -0700

    update

--- prep/day01/Strings/Main.java
+++ prep/day01/Strings/Main.java
@@ -6,10 +6,12 @@ public class Main {
         byte[] mybytes = {67, 89, 110, 65, 68};
 
         String mystring = new String(mybytes); // 0100 0011
+        char star = '*';
 
-        System.out.println(mystring);
-        System.out.println(Integer.toBinaryString(67));
-        System.out.printf("%c\n", 65);
+        // System.out.println(mystring);
+        System.out.println(Integer.toBinaryString(star));
+        System.out.println(Integer.toBinaryString(42));
+        // System.out.printf("%c\n", 65);
 
         // for(int i = 0; i < 300; ++i){
         //     System.out.printf("%d = %c and binary is %s\n",i, i, Integer.toBinaryString(i));
