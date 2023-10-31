a4f7fa63811892dc5ccee7fac1aee160b7eef5fe
tmax818 <tylermaxwell661@gmail.com>
2023-10-31 07:21:11 -0700

    update algo

---
+++ algorithms/day07/index.js
@@ -0,0 +1,228 @@
+/**
+ * A class to represents a single item of a SinglyLinkedList that can be
+ * linked to other Node instances to form a list of linked nodes.
+ */
+class ListNode {
+    /**
+     * Constructs a new Node instance. Executed when the 'new' keyword is used.
+     * @param {any} data The data to be added into this new instance of a Node.
+     *    The data can be anything, just like an array can contain strings,
+     *    numbers, objects, etc.
+     * @returns {ListNode} This new Node instance is returned automatically without
+     *    having to be explicitly written (implicit return).
+     */
+    constructor(data) {
+      this.data = data;
+      /**
+       * This property is used to link this node to whichever node is next
+       * in the list. By default, this new node is not linked to any other
+       * nodes, so the setting / updating of this property will happen sometime
+       * after this node is created.
+       *
+       * @type {ListNode|null}
+       */
+      this.next = null;
+    }
+  }
+  
+  /**
+   * This class keeps track of the start (head) of the list and to store all the
+   * functionality (methods) that each list should have.
+   */
+  class SinglyLinkedList {
+    /**
+     * Constructs a new instance of an empty linked list that inherits all the
+     * methods.
+     * @returns {SinglyLinkedList} The new list that is instantiated is implicitly
+     *    returned without having to explicitly write "return".
+     */
+    constructor() {
+      /** @type {ListNode|null} */
+      this.head = null;
+    }
+  
+    /**
+     * Determines if this list is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+      return this.head === null;
+    }
+  
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(n) linear, n = length of list.
+     * - Space: O(1) constant.
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBack(data) {
+      const newBack = new ListNode(data);
+  
+      if (this.isEmpty()) {
+        this.head = newBack;
+        return this;
+      }
+  
+      let runner = this.head;
+  
+      while (runner.next !== null) {
+        runner = runner.next;
+      }
+  
+      runner.next = newBack;
+      return this;
+    }
+  
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(n) linear, n = length of list.
+     * - Space: O(n) linear due to the call stack.
+     * @param {any} data The data to be added to the new node.
+     * @param {?ListNode} runner The current node during the traversal of this list
+     *    or null when the end of the list has been reached.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBackRecursive(data, runner = this.head) {
+      if (this.isEmpty()) {
+        this.head = new ListNode(data);
+        return this;
+      }
+  
+      if (runner.next === null) {
+        runner.next = new ListNode(data);
+        return this;
+      }
+      return this.insertAtBackRecursive(data, runner.next);
+    }
+  
+    /**
+     * Calls insertAtBack on each item of the given array.
+     * - Time: O(n * m) n = list length, m = arr.length.
+     * - Space: O(1) constant.
+     * @param {Array<any>} vals The data for each new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBackMany(vals) {
+      for (const item of vals) {
+        this.insertAtBack(item);
+      }
+      return this;
+    }
+  
+    /**
+     * Converts this list into an array containing the data of each node.
+     * - Time: O(n) linear.
+     * - Space: O(n).
+     * @returns {Array<any>} An array of each node's data.
+     */
+    toArr() {
+      const arr = [];
+      let runner = this.head;
+  
+      while (runner) {
+        arr.push(runner.data);
+        runner = runner.next;
+      }
+      return arr;
+    }
+  
+    /**
+     * Determines the length of this list.
+     * - Time: O(n) linear, n = length of list
+     * - Space: O(1) constant
+     * @returns {number} The length.
+     */
+    length() {
+      let len = 0;
+      let runner = this.head;
+  
+      while (runner) {
+        len += 1;
+        runner = runner.next;
+      }
+      return len;
+    }
+  /**
+     * Creates a new node with the given data and inserts that node at the front
+     * of the list.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} data The data for the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+  insertAtFront(data) {
+    const newHead = new ListNode(data);
+    newHead.next = this.head;
+    this.head = newHead;
+    return this;
+  }
+
+  /**
+   * Removes the first node of this list.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {any} The data from the removed node.
+   */
+  removeHead() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const oldHead = this.head;
+    this.head = oldHead.next;
+    return oldHead.data;
+  }
+
+  /**
+   * Calculates the average of this list.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @returns {number|NaN} The average of the node's data.
+   */
+  average() {
+    let runner = this.head;
+    let sum = 0;
+    let cnt = 0;
+
+    while (runner) {
+      cnt++;
+      sum += runner.data;
+      runner = runner.next;
+    }
+
+    /**
+     * Dividing by 0 will give you NaN (Not a Number), so an empty list
+     * will return NaN in this case, it may make sense to allow NaN to be
+     * returned, because the average of an empty list doesn't make sense and
+     * it could be misleading to return 0 since 0 is the average of any
+     * list with a sum of 0 (due to negatives or all zeros).
+     */
+    return sum / cnt;
+  }
+
+  /**
+   * Removes the last node of this list.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @returns {any} The data from the node that was removed.
+   */
+  removeBack() {}
+
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @param {any} val The data to search for in the nodes of this list.
+   * @returns {boolean}
+   */
+  contains(val) {}
+
+  }
+  
+
