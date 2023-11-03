7843a319d8bb943e15ef9a7c68095fd303749153
tmax818 <tylermaxwell661@gmail.com>
2023-11-03 07:56:04 -0700

    add todos

--- code/log-reg-prep/src/main/java/co/tylermaxwell/logregprep/services/UserService.java
+++ code/log-reg-prep/src/main/java/co/tylermaxwell/logregprep/services/UserService.java
@@ -1,5 +1,8 @@
 package co.tylermaxwell.logregprep.services;
 
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
@@ -17,10 +20,45 @@ public class UserService {
     // TO-DO: Write register and login methods!
     public User register(User newUser, BindingResult result) {
         // TO-DO: Additional validations!
+         //TODO -  Reject if email is taken (present in database)
+
+     
+        
+        //TODO -  Reject if password doesn't match confirmation
+        
+    
+        
+        //TODO -  Return null if result has errors
+
+    
+        //TODO -  Hash passowrd 
+    
+        
+        //TODO set password
+        
+
+        //TODO - save user to database
+
         return null;
     }
+
+
+
+
+
     public User login(LoginUser newLoginObject, BindingResult result) {
         // TO-DO: Additional validations!
+        
+        //TODO Find user in the DB by email
+      
+
+        //TODO Reject if NOT present
+        //TODO Reject if BCrypt password match fails
+ 
+    
+        //TODO  Return null if result has errors
+
+        //TODO Otherwise, return the user object
         return null;
     }
 }
