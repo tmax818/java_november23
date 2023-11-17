d8adc58b73d7f4112b76db2a88db76005637d845
tmax818 <tylermaxwell661@gmail.com>
2023-11-17 07:53:44 -0800

    up

--- algorithms/day19/index.js
+++ algorithms/day19/index.js
@@ -54,34 +54,34 @@ class DoublyLinkedList {
       return this.head === null;
     }
 
-    /**
- * Inserts a new node with the given newVal after the node that has the
- * given targetVal as it's data.
- * - Time: O(?).
- * - Space: O(?).
- * @param {any} targetVal The node data to find.
- * @param {any} newVal Data for the new node.
- * @returns {boolean} Indicates if the new node was added.
- */
-insertAfter(targetVal, newVal) {}
-
-/**
- * Inserts a new node with the given newVal before the node that has the
- * given targetVal as it's data.
- * - Time: O(?).
- * - Space: O(?).
- * @param {any} targetVal The node data to find.
- * @param {any} newVal Data for the new node.
- * @returns {boolean} Indicates if the new node was added.
- */
-insertBefore(targetVal, newVal) {}
-  
-    /**
-     * Converts this list to an array of the node's data.
-     * - Time: O(n) linear, n = list length.
-     * - Space: O(n) linear, array grows as list length increases.
-     * @returns {Array<any>} All the data of the nodes.
+        /**
+     * Inserts a new node with the given newVal after the node that has the
+     * given targetVal as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
      */
+    insertAfter(targetVal, newVal) {}
+
+    /**
+     * Inserts a new node with the given newVal before the node that has the
+     * given targetVal as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
+     */
+    insertBefore(targetVal, newVal) {}
+      
+        /**
+         * Converts this list to an array of the node's data.
+         * - Time: O(n) linear, n = list length.
+         * - Space: O(n) linear, array grows as list length increases.
+         * @returns {Array<any>} All the data of the nodes.
+         */
     toArray() {
       const vals = [];
       let runner = this.head;
